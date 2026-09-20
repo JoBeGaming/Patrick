dc1947f54d9c974e120c1bc66ffb889548df3a98
JoBe <165585785+JoBeGaming@users.noreply.github.com>
2026-09-20 21:55:35 +0200

    [Fix]: Remove quotes around `user_log_repr` in `errorhandler.py`

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -34,7 +34,7 @@ class ErrorHandler(commands.Cog):
             )
         elif isinstance(error, commands.CommandNotFound):
             self.bot.logger.info(
-                f"User '{user_log_repr(ctx.author)}' attempted to run an unrecognized command: '{ctx.message.content[1:]}'"
+                f"User {user_log_repr(ctx.author)} attempted to run an unrecognized command: '{ctx.message.content[1:]}'"
             )
             await respond("Unrecognized command :'(")
         elif isinstance(error, commands.CommandOnCooldown):
